6523ed1d1f657aff85cbb2595a69ced3634736bc
Suhas P <sup@akamai.com>
2021-07-09 17:07:53 +0530

    preload lcp image only

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,7 @@ router.get("/", async (req, res) => {
     const products = await Product.find({})
       .sort("-createdAt")
       .populate("category");
-    res.append('Link', ['https://ka-f.fontawesome.com/releases/v5.15.3/webfonts/free-fa-solid-900.woff2; rel="preload" as="font"',
-      'https://ka-f.fontawesome.com/releases/v5.15.3/webfonts/free-fa-brands-400.woff2; rel="preload" as="font"',
-      'https://ka-f.fontawesome.com/releases/v5.15.3/webfonts/free-fa-regular-400.woff2; rel="preload" as="font"',
-      '</javascripts/displayNavigationTiming.js>; rel="preload" as="script"',
-      '</javascripts/main.js>; rel="preload" as="script"',
-      '</javascripts/displayResourceLoadTime.js>; rel="preload" as="script"',
-      '</javascripts/displayResourceSize.js>; rel="preload" as="script"',
-      '</javascripts/search.js>; rel="preload" as="script"',
-      '</stylesheets/style.css>; rel="preload" as="style"',
+    res.append('Link', [
       '</images/slide1.jpg>; rel="preload" as="image"'
     ])
     res.render("shop/home", { pageName: "Home", products });    
