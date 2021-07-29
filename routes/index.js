3f8b78db9d9508cb2a2d812c2792723febf00673
Suhas P <sup@akamai.com>
2021-07-29 10:49:28 +0530

    loading push files during startup

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.use(csrfProtection);
 let dependencies = ['/javascripts/main.js','/javascripts/search.js','/javascripts/displayNavigationTiming.js','/javascripts/displayResourceLoadTime.js','/javascripts/displayResourceSize.js', '/stylesheets/style.css']
 let dependencyType = ['application/javascript', 'application/javascript', 'application/javascript', 'application/javascript', 'application/javascript', 'text/css']
 let filesToRead = dependencies.map( (dep) => fs.readFileAsync(`${__dirname}/../public${dep}`))
-let abc = await Promise.all(filesToRead)
+let abc = Promise.all(filesToRead)
 console.log("filesToRead " + filesToRead)
 // // GET: home page
 router.get("/", async (req, res) => {
